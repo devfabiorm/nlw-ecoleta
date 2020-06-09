728f8a765603cec08e2eb67ba36c6c2b708d2a87
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-06-08 23:58:20 -0300

    FAzendo upload de imagem

--- aulas/mobile/src/pages/Points/index.tsx
+++ aulas/mobile/src/pages/Points/index.tsx
@@ -19,6 +19,7 @@ interface Point {
     id: number;
     name: string;
     image: string;
+    image_url: string;
     latitude: number;
     longitude: number;
 }
@@ -136,7 +137,7 @@ const Points = () => {
                                     }}
                                 >
                                     <View style={styles.mapMarkerContainer}>
-                                        <Image source={{ uri: point.image }} style={styles.mapMarkerImage} />
+                                        <Image source={{ uri: point.image_url }} style={styles.mapMarkerImage} />
                                         <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                                     </View>
                                 </Marker>
--- aulas/web/src/pages/CreatePoint/index.tsx
+++ aulas/web/src/pages/CreatePoint/index.tsx
@@ -4,6 +4,7 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
+import Dropzone from '../../components/Dropzone';
 
 import api from '../../services/api';
 
@@ -44,6 +45,7 @@ const CreatePoint = () => {
     const [selectedCity, setSeletedCity] = useState('');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -99,7 +101,7 @@ const CreatePoint = () => {
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-        const { name, value} = event.target;
+        const { name, value } = event.target;
 
         setFormData({
             ...formData,
@@ -111,16 +113,16 @@ const CreatePoint = () => {
 
         const alreadySeleted = selectedItems.findIndex(item => item === id);
 
-        if(alreadySeleted >= 0) {
+        if (alreadySeleted >= 0) {
 
             const filteredItems = selectedItems.filter(item => item !== id);
             setSelectedItems(filteredItems);
 
-        }else{
+        } else {
             setSelectedItems([...selectedItems, id]);
         }
 
-        
+
     }
 
     async function handleSubmit(event: FormEvent) {
@@ -132,23 +134,30 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+
+
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if (selectedFile) {
+            data.append('image', selectedFile);
+        }
+
 
         //console.log(data)
-       await api.post('points', data);
+        await api.post('points', data);
 
-       alert('Ponto de colega criado!');
+        alert('Ponto de colega criado!');
 
-       history.push('/');
+        history.push('/');
     }
 
     return (
@@ -164,6 +173,8 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
