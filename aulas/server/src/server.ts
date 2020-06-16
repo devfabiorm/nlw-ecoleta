4bf525ca74b439f397472ca2676a8ee10a9aa74c
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-06-15 21:20:13 -0300

    Validando campos de entrada no cadastro de pontos

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -2,6 +2,7 @@ import express, { response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(express.json()); //Serve para adicionar uma funcionalidade a aplicação
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
